05fd3f63d8e52da2027f04187464688d83e4ca6f
Jahid1604091 <jh409780@gmail.com>
2022-01-24 22:03:57 +0600

    done

--- src/Categories.js
+++ src/Categories.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Categories = () => {
-  return <h2>categories component</h2>;
+const Categories = ({filterItems,categories}) => {
+  return <div className='btn-container'>
+    {
+      categories.map((c,idx)=>{
+       return <button key={idx} className='filter-btn' onClick={()=>filterItems(c)}>{c}</button>
+      })
+    }
+  </div>;
 };
 
 export default Categories;
